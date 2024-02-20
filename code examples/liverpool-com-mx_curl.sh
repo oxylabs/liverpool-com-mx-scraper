ba38ab451bc68801aa53eaacef1c6b94f23a8253
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:54:49 +0200

    Adding code examples/liverpool-com-mx_curl.sh

---
+++ code examples/liverpool-com-mx_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.liverpool.com.mx/tienda/colchones/n-wg53pt7492dst%2f0p0r%2fpz0kb9npyijqg5tzypy%2fduqodvgzhzvmk%2fi35f6qjb7l6"}'
